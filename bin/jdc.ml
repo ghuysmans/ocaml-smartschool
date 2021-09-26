494f541e359aa194bdb3c9cb607a81c1ac9c9cb4
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-09-26 02:58:52 +0200

    Print the day of the week

--- bin/jdc.ml
+++ bin/jdc.ml
@@ -1,12 +1,43 @@
 open Smartschool.Ical
 
+let weekday {Glical.Datetime.year; month; day; _} =
+  let open Unix in
+  let tm = {
+    tm_sec = 0; tm_min = 0; tm_hour = 0;
+    tm_mday = day; tm_mon = month - 1; tm_year = year - 1900;
+    tm_wday = 0; tm_yday = 0; tm_isdst = false;
+  } in
+  match (snd @@ mktime tm).tm_wday with
+  | 0 -> "Sun"
+  | 1 -> "Mon"
+  | 2 -> "Tue"
+  | 3 -> "Wed"
+  | 4 -> "Thu"
+  | 5 -> "Fri"
+  | 6 -> "Sat"
+  | _ -> failwith "mktime"
+
+
 let () =
-  file_contents Sys.argv.(1) |> parse |>
-  List.iter (fun {dtstart; dtend; summary; location} ->
-    Printf.printf "%s %s -> %s\t%s%s\n"
-      (string_of_date dtstart) (string_of_time dtstart) (string_of_time dtend)
-      summary
-      (match location with
-       | None -> ""
-       | Some l -> " @ " ^ l)
+  let l = Glical.file_contents Sys.argv.(1) |> parse |> of_smartschool in
+  let show f lbl =
+    print_endline @@ lbl ^ ":";
+    List.map (fun x -> "- " ^ f x) l |>
+      List.sort_uniq compare |>
+      List.iter print_endline;
+    print_newline ()
+  in
+  show (fun {class_; _} -> class_) "Classes";
+  show (fun {subject; _} -> subject) "Subjects";
+  show (fun {room; _} -> room) "Rooms";
+  List.sort compare l |>
+  List.iter (fun {event; class_; room; subject} ->
+    Printf.printf "%s %s %s -> %s\t%s\t%s\t%s\n"
+      (weekday event.dtstart)
+      (string_of_date event.dtstart)
+      (string_of_time event.dtstart)
+      (string_of_time event.dtend)
+      class_
+      room
+      subject
   )
