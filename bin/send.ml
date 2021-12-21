dd6e7395085c837f73f5668168a03923a68bc88d
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-12-21 04:42:19 +0100

    WIP read a CSV and transform a Markdown template

--- bin/send.ml
+++ bin/send.ml
@@ -1,7 +1,26 @@
 open Smartschool_soap
 
 let () = Lwt_main.run (
-  send_message ~access_code:(read_line ())
-               ~from:"huysgu" ~to_:("huysgu", 0)
-               ~title:"send.ml" Sys.argv.(1)
+  match Sys.argv with
+  | [| _; "-data"; d; "-template"; t |] ->
+    begin match Csv.load d with
+    | [] -> Lwt.return_unit
+    | header :: data ->
+      let access_code = read_line () in
+      let h = Hashtbl.create 10 in
+      header |> List.iteri (fun i v -> Hashtbl.replace h v i);
+      let template = Omd.of_channel (open_in t) in
+      data |> Lwt_list.iter_s (fun row ->
+        let get x = Hashtbl.find h x |> List.nth row in
+        let to_ = get "dest", int_of_string (get "dest_n") in
+        let title = get "title" in
+        let%lwt () = Lwt_io.printl (fst to_) in
+        List.map (Merge_omd.map get) template |>
+        Omd.to_html |>
+        send_message ~access_code ~from:"huysgu" ~to_ ~title
+      )
+    end
+  | _ ->
+    Printf.eprintf "usage: %s -data data.csv -template tpl.md\n" Sys.argv.(0);
+    exit 1
 )
