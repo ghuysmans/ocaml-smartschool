ce4e4dafb6c063b309e563ef7123fa5853481144
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-03-27 21:15:39 +0200

    WIP dump accounts and coaccounts

--- bin/secondary.ml
+++ bin/secondary.ml
@@ -6,26 +6,51 @@ let () = Lwt_main.run (
   | Error e -> failwith (Protocol_conv_json.Json.error_to_string_hum e)
   | Ok l ->
     let open Smartschool.Users in
+    let out = Csv.to_channel stdout in
+    Csv.output_record out [
+      "type";
+      "official_class"; "lastname"; "firstname";
+      "role"; "rel_lastname"; "rel_firstname";
+      "rel_coaccount";
+      "error"
+    ];
+    let dummy = {
+      Coaccount.firstname = "";
+      lastname = "";
+      email = "";
+      email_verified = false;
+      phone_number = "";
+      mobile_number = "";
+      typ = "";
+      authenticator_app = false;
+      yubikey = false;
+      status = "";
+    } in
     l |> List.iter (fun u ->
       let class_ = (List.find (fun g -> g.is_class) u.groups).code in
-      let show (c : Coaccount.t) msg =
-        Printf.printf "%s %s*%s, %s*%s (%s) : %s\n"
-          class_ u.lastname u.firstname
-          c.lastname c.firstname c.typ
+      let show typ i (c : Coaccount.t) msg =
+        Csv.output_record out [
+          typ;
+          class_; u.lastname; u.firstname;
+          c.typ; c.lastname; c.firstname;
+          string_of_int (i + 1);
           msg
+        ]
       in
       let find_co f msg =
         let rec g i =
           if i = Array.length u.coaccounts then
             ()
           else if u.coaccounts.(i).typ <> "" && f u.coaccounts.(i) then
-            show u.coaccounts.(i) msg
+            show "lint" i u.coaccounts.(i) msg
           else
             g (i + 1)
         in
         g 0
       in
       let open Coaccount in
+      show "dump" (-1) dummy "";
+      Array.iteri (fun i c -> if c.typ <> "" then show "dump" i c "") u.coaccounts;
       find_co (fun c -> c.typ = "Sélectionner") "type";
       let find_co_n f = find_co (fun c -> f c.firstname || f c.lastname) in
       find_co_n ((=) "") "nom vide";
@@ -38,15 +63,16 @@ let () = Lwt_main.run (
         List.length > 1
       ) "nom long";
       Array.to_list u.coaccounts |>
-        List.filter (fun c -> c.typ <> "") |>
-        List.sort (fun c c' ->
+        List.mapi (fun i x -> i, x) |>
+        List.filter (fun (_, c) -> c.typ <> "") |>
+        List.sort (fun (_, c) (_, c') ->
           let f x = x.lastname, x.firstname in
           compare (f c) (f c')
         ) |>
-        List.fold_left (fun acc c ->
+        List.fold_left (fun acc (i, c) ->
           match acc with
           | None -> Some c
-          | Some c' -> if c = c' then show c "doublon"; Some c
+          | Some c' -> if c = c' then show "lint" i c "doublon"; Some c
         ) None |>
         ignore
     );
