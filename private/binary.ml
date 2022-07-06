ee8138d8b8d0c1a0e9a0c08730820ce51da22956
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-07-06 21:03:57 +0200

    Implement Binary.param

--- private/binary.ml
+++ private/binary.ml
@@ -1,9 +1,17 @@
 type t = bool
 
+let of_string = function
+  | "0" -> false
+  | "1" -> true
+  | _ -> failwith "Binary.of_string"
+
 let of_xml_light_exn = function
-  | Xml.(Element (_, _, [PCData "0"])) -> false
-  | Xml.(Element (_, _, [PCData "1"])) -> true
+  | Xml.(Element (_, _, [PCData x])) -> of_string x
   | _ -> failwith "Binary.of_xml_light_exn"
 
+let to_string b = if b then "1" else "0"
+
 let to_xml_light b =
-  Xml.(Element ("status", [], [PCData (if b then "1" else "0")]))
+  Xml.Element ("status", [], [Xml.PCData (to_string b)])
+
+let params = Params.Simple {fwd = to_string; bwd = of_string}
